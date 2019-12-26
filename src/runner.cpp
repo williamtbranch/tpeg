dcedd920c158fbeae32e5294def5e58e0b7160ac
Bill Branch <williamtbranch@gmail.com>
2019-12-26 09:50:27 -0600

    Basic test parser functionality finished

--- src/runner.cpp
+++ src/runner.cpp
@@ -20,9 +20,34 @@ int main (int argc, char *argv[]){
 
     //Create test batch object
     testBatch *tests = new testBatch(testString);
+    testBatch*& myTests = tests;
 
     if (tests->valid == false) {
         std::cout << tests->batch_error.errorMSG << std::endl;
+    }
+
+    //walk tree and print content
+    for (int i = 0; tests->batch.size() > i; i++ ){
+
+        std::cout << tests->batch[i].testName << std::endl;
+        for (int x = 0; tests->batch[i].grammar.expressions.size() > x; x++){
+            std::cout << "  " << tests->batch[i].grammar.expressions[x].name
+            << " <- "
+            << tests->batch[i].grammar.expressions[x].rule << std::endl;
+
+        }
+        std::cout << std::endl;
+
+        for (int x = 0; tests->batch[i].tests.size() > x; x++){
+            if (tests->batch[i].tests[x].type  == testType::MATCH) {
+                std::cout << "  " << "Match" << std::endl;
+            }
+            else{
+                std::cout << "  " << "Search" << std::endl;
+            }
+            std::cout << "  " << tests->batch[i].tests[x].input << std::endl;
+            std::cout << "  " << tests->batch[i].tests[x].expected << std::endl << std::endl;
+        }
     }
 
     delete tests;
